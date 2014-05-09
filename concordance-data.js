82737b789c902a55bb019a35e70a815ac7539f32
Isaac Hodes <ihodes@mac.com>
2014-05-09 11:22:01 -0400

    Delete concordance-data.js
    replaced by output.js for now

--- concordance-data.js
+++
@@ -1,2 +0,0 @@
-var data = {"vcftest2,training.mutect.20140505,truth.chr20": 372, "truth.chr20,training.mutect.20140505": 355, "truth.chr20,vcftest1,training.mutect.20140505": 347, "truth.chr20": 123, "vcftest1,training.mutect.20140505": 65, "vcftest2,training.mutect.20140505": 62, "vcftest2,vcftest1,training.mutect.20140505,truth.chr20": 330, "vcftest2,vcftest1,training.mutect.20140505": 64, "training.mutect.20140505": 58};
-var callers = ["vcftest2", "vcftest1", "training.mutect.20140505", "truth.chr20"];
