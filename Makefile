2deaf65a641cefe2e2c82743cb9df2ba6f4629db
Dan Vanderkam <danvdk@gmail.com>
2014-05-20 16:08:18 -0400

    Add <div> to jsdom and test more thoroughly with jQuery

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
+.PHONY: test
+
 test:
 	mocha
--- test/simple.js
+++ test/simple.js
@@ -1,5 +1,6 @@
 var assert = require("assert"),
-    jsdom = require("jsdom").jsdom;
+    jsdom = require("jsdom").jsdom,
+    _ = require("underscore");
 
 // TODO(danvk): figure out if it's possible to use Node's require() here.
 // TODO(danvk): wrap this up in a test helper module.
@@ -10,6 +11,7 @@ var document = jsdom('<html><head>' +
     '</head><body></body></html>');
 
 var window = document.parentWindow;
+var $ = require("jquery")(window);
 
 describe('d3.stacked-bar-chart', function() {
   it('should have the right defaults', function() {
@@ -20,14 +22,15 @@ describe('d3.stacked-bar-chart', function() {
 
   it('should be minimally functional', function() {
     var data = [
-        [{"name":"3","value":1325,"y0":0,"y1":1325},{"name":"2","value":98,"y0":1325,"y1":1423},{"name":"1","value":104,"y0":1423,"y1":1527}],
-        [{"name":"3","value":1325,"y0":0,"y1":1325},{"name":"2","value":260,"y0":1325,"y1":1585},{"name":"1","value":68,"y0":1585,"y1":1653}],
-        [{"name":"3","value":1325,"y0":0,"y1":1325},{"name":"2","value":200,"y0":1325,"y1":1525},{"name":"1","value":155,"y0":1525,"y1":1680}]
+        [{"name":"3","value":1325},{"name":"2","value":98},{"name":"1","value":104}],
+        [{"name":"3","value":1325},{"name":"2","value":260},{"name":"1","value":68}],
+        [{"name":"3","value":1325},{"name":"2","value":200},{"name":"1","value":155}]
     ];
     data[0].name = 'truth.chr20';
     data[1].name = 'mutect.chr20';
     data[2].name = 'somaticsniper.chr20';
 
+    var sum = function(list) { return list.reduce(function(a,b){return a + b;}, 0); };
     var chart = window.d3.chart.stackedBars()
         .title("Caller Concordance")
         .yAxisTitle("Number of Variants Called")
@@ -37,13 +40,21 @@ describe('d3.stacked-bar-chart', function() {
            return parseInt(b.name) > parseInt(a.name);
         })
         .barComparator(function(a, b) {
-            var totalA = _.sum(a.map(function(s){ return s.value; }));
-            var totalB = _.sum(b.map(function(s){ return s.value; }));
+            var totalA = sum(a.map(function(s){ return s.value; }));
+            var totalB = sum(b.map(function(s){ return s.value; }));
             return totalA - totalB;
         });
 
     var div = document.createElement('div');
     div.setAttribute('id', 'conc_div');
+    document.body.appendChild(div);
     window.d3.select('#conc_div').datum(data).call(chart);
+
+    var sumTexts = $.makeArray($('g.bar text', div)).map(function(x) { return $(x).text()});
+    assert.deepEqual(["1,527", "1,653", "1,680"], sumTexts);
+
+    // TODO: add more thorough assertions here.
   });
+
+  // TODO: add tests of more complex behavior.
 });
