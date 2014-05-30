4405fa708961e0a05bd8f11179c9a36f764bf584
Dan Vanderkam <danvdk@gmail.com>
2014-05-29 22:52:44 -0400

    record disagreements

--- diff_vcfs.py
+++ diff_vcfs.py
@@ -24,12 +24,24 @@ def is_ok(call):
     return call.FILTER == PASS or not call.FILTER
 
 
+def sample(r, items, count):
+    if len(items) < count:
+        return items
+    else:
+        return r.sample(items, count)
+
+
 def main(args):
     assert len(args) == 2
     vcfs = [vcf.Reader(open(path)) for path in args]
 
-    a, b = [{call_key(call) for call in reader if is_ok(call)}
-            for reader in vcfs]
+    a_map, b_map = [{call_key(call): call.ALT for call in reader if is_ok(call)}
+                    for reader in vcfs]
+
+    a = set(a_map.keys())
+    b = set(b_map.keys())
+
+    disagreement_loci = [k for k in a.intersection(b) if a_map[k] != b_map[k]]
 
     r = random.Random()
     print '''Sources:
@@ -43,11 +55,13 @@ A`B: %(countAminusB)d
 B`A: %(countBminusA)d
 A^B: %(countAintB)d
 A+B: %(countAunionB)d
+Disagreements: %(countDisagreements)d
 
 Pairwise samples:
 A`B: %(sampleAminusB)s
 B`A: %(sampleBminusA)s
 A^B: %(sampleAintB)s
+Disagreements: %(sampleDisagreements)s
 ''' % {'sourceA': args[0],
        'sourceB': args[1],
        'countA': len(a),
@@ -56,9 +70,11 @@ A^B: %(sampleAintB)s
        'countBminusA': len(b - a),
        'countAintB': len(a.intersection(b)),
        'countAunionB': len(a.union(b)),
-       'sampleAminusB': ', '.join(r.sample(a - b, 10)),
-       'sampleBminusA': ', '.join(r.sample(b - a, 10)),
-       'sampleAintB': ', '.join(r.sample(a.intersection(b), 10))}
+       'sampleAminusB': ', '.join(sample(r, a - b, 10)),
+       'sampleBminusA': ', '.join(sample(r, b - a, 10)),
+       'sampleAintB': ', '.join(sample(r, a.intersection(b), 10)),
+       'countDisagreements': len(disagreement_loci),
+       'sampleDisagreements': ', '.join(sample(r, disagreement_loci, 10))}
 
 
 if __name__ == '__main__':
