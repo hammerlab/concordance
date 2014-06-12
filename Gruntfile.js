897d184eb1508eb6dbf3baa891b7b5096570d83b
Dan Vanderkam <danvdk@gmail.com>
2014-06-12 15:53:50 -0400

    fix Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,25 @@
 /*global module*/
 module.exports = function (grunt) {
   'use strict';
- 
+
   var gruntConfig = {};
   grunt.loadNpmTasks('grunt-contrib-qunit');
   gruntConfig.qunit = {
-      src: ['test/index.html']
+    src: ['test/index.html']
   };
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   gruntConfig.jshint = {
-      options: {
-          jshintrc: '.jshintrc',
-      },
-      allFiles: [
-          'Gruntfile.js',
-          'viewer/**/*.js',
-          'test/**/*.js'
-      ]
+    options: {
+      jshintrc: '.jshintrc'
+    },
+    allFiles: [
+      'Gruntfile.js',
+      'viewer/**/*.js',
+      'test/**/*.js'
+    ]
   };
-  
+
   grunt.registerTask('test', 'qunit:src');
   grunt.registerTask('travis', ['lint', 'test']);
   grunt.initConfig(gruntConfig);
