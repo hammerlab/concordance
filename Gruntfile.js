863549a2e07fbef3803efdd41d291fe196fc00cd
Dan Vanderkam <danvdk@gmail.com>
2014-06-12 15:49:09 -0400

    .jshintrc

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,9 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   gruntConfig.jshint = {
       options: {
-        indent: 2
-        /*bitwise: true, camelcase: true, curly: true, eqeqeq: true, forin: true, immed: true,
-          indent: 4, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, plusplus: true,
-          quotmark: true, regexp: true, undef: true, unused: true, strict: true, trailing: true,
-          maxparams: 3, maxdepth: 2, maxstatements: 50
-          */},
-      all: [
+          jshintrc: '.jshintrc',
+      },
+      allFiles: [
           'Gruntfile.js',
           'viewer/**/*.js',
           'test/**/*.js'
