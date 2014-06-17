ac15768a79bd8d16bd473c2cc3239afd99d30a7c
Dan Vanderkam <danvdk@gmail.com>
2014-06-17 19:06:30 -0400

    instructions

--- pdiff-tests/test.js
+++ pdiff-tests/test.js
@@ -1,11 +1,16 @@
 var pdifftest = require('./lib/pdifftest');
 
+// 1 = number of tests in this suite.
 casper.test.begin('Concordance example', 1, function(test) {
+  // This sequence just configures the test, it doesn't run it.
   pdifftest.startTest(casper, 'viewer/concordance.html', function() {
+    // ... click things ...
+
     var screenshot = pdifftest.takeScreenshot(casper);
     pdifftest.checkAgainstGolden(casper, screenshot, 'pdiff-tests/golden/concordance.png');
   });
 
+  // Until you call "test.done()", the test will hang!
   casper.run(function() {
     test.done();
   });
